c3422d70d9c7d2319b6a7cb5faa0424995327c0b
Arslan <arslansultanbekuulu@gmail.com>
2020-03-20 21:24:50 +0600

    Heroku Deployment

--- Courses_Django/settings.py
+++ Courses_Django/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASS'),
         'HOST': config("DB_HOST"),
-        'PORT': '',
+        'PORT': 5432,
         'ATOMIC_REQUESTS': True,
     }
 }
